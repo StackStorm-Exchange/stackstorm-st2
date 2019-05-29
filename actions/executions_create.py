b213ad89c3436f6a94fccf41f20b8ec4c9edeb88
John Arnold (AZURE) <johnar@microsoft.com>
2019-05-29 23:28:10 +0000

    add explicit parameters

--- actions/executions_create.py
+++ actions/executions_create.py
@@ -15,7 +15,7 @@ def format_result(item):
 
 
 class St2ExecutionsCreateAction(St2BaseAction):
-    def run(self, action, parameters=None):
+    def run(self, action, parameters=None, user=None, delay=None, trace_id=None, trace_tag=None):
         parameters = parameters or {}
 
         action_resource = self.client.managers['Action'].get_by_ref_or_id(action)
@@ -28,17 +28,17 @@ class St2ExecutionsCreateAction(St2BaseAction):
         execution.action = action_ref
         execution.parameters = parameters
 
-        if parameters.get('user'):
-            execution.user = parameters['user']
+        if user:
+            execution.user = user
 
-        if parameters.get('delay'):
-            execution.delay = parameters['delay']
+        if delay:
+            execution.delay = delay
 
-        if not parameters.get('trace_id') and parameters.get('trace_tag'):
-            execution.context = {'trace_context': {'trace_tag': parameters['trace_tag']}}
+        if not trace_id and trace_tag:
+            execution.context = {'trace_context': {'trace_tag': trace_tag}}
 
-        if parameters.get('trace_id'):
-            execution.context = {'trace_context': {'id_': parameters['trace_id']}}
+        if trace_id:
+            execution.context = {'trace_context': {'id_': trace_id}}
 
         result = self.client.executions.create(instance=execution)
         result = format_result(item=result)
